379dc0bba34363d3b51f07d4904f057fedc15950
Nicholas Dewberry <coolguynickyd@gmail.com>
2025-01-25 14:25:25 -0500

    added player damage dealing to enemies, fixed sound effect

--- ggj2025/PlayerStuff/playerattacks/player_attack_1_manager.gd
+++ ggj2025/PlayerStuff/playerattacks/player_attack_1_manager.gd
@@ -6,14 +6,9 @@ extends Node2D
 var attack1speed: float = 90
 @export var attack1attacktimer: Timer = null
 @export var attack1baseobject: PackedScene = null
+#TODO - ADD MORE AUDIO WITH ENEMIES 
+@export var attacksoundeffect: AudioStreamPlayer2D = null
 
-@export var mainattackaudio: PackedScene = null
-@export var popsound1: AudioStream  = null
-@export var popsound2: AudioStream = null
-@export var popsound3: AudioStream = null
-@export var popsound4: AudioStream = null
-@export var popsound5: AudioStream = null
-@export var popsound6: AudioStream = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -30,25 +25,10 @@ func reassignvalues():
 
 func _on_attack_1_timer_timeout() -> void:
 	#Throw out the bubble attack 
-	#Play random sound effect - NOTE: NOW ITS JUST 1 SOUND EFFECT! LATER WE WILL HAVE MORE 
-	var playthissound = randi_range(1,6)
-	var mypopsound = mainattackaudio.instantiate()
-	match playthissound:
-		1:
-			mypopsound.stream = popsound1
-		2:
-			mypopsound.stream = popsound2
-		3:
-			mypopsound.stream = popsound3
-		4:
-			mypopsound.stream = popsound4
-		5:
-			mypopsound.stream = popsound5
-		6:
-			mypopsound.stream = popsound6
-	add_child(mypopsound)
-	mypopsound.play()
+	#Play random sound effect 
+	
 	var myattack = attack1baseobject.instantiate()
+	myattack.damage = PlayerSingleton.baseattack1damage
 	myattack.transform = get_parent().global_transform
 	get_parent().get_parent().add_child(myattack)
 	attack1attacktimer.wait_time = PlayerSingleton.baseattack1timer
